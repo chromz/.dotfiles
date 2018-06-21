d75d19f4c62e71cb1778855b799ae22fd91101d3
José Rodrigo Custodio <jorocuva@gmail.com>
2018-06-20 22:21:22 -0600

    Border

--- st/config.h
+++ st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "DejaVuSansMono Nerd Font:size=9:antialias=true:autohint=true";
-static int borderpx = 0;
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
