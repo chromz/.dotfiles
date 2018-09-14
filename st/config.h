70208ebbadda3ea3fdcfc6b9be22b337f1227e40
Rodrigo Custodio <jorocuva@gmail.com>
2018-09-14 17:53:49 -0600

    Changed font

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DankMono Nerd Font:size=10:antialias=true:autohint=true";
+static char *font = "DejaVuSansMono Nerd Font:size=9:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
