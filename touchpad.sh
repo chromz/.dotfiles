9fbae98a4c8703f2eda3a7cfec82217719e35645
Rodrigo Custodio <jorocuva@gmail.com>
2018-11-02 08:42:32 -0600

    touchpad script

--- touchpad.sh
+++ touchpad.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 touchpadname="$(xinput list | awk -F"[ ]+" '/Touch/{printf("%s %s\n", $3, $4)}')"
-echo $touchpadname
+isenabled="$(xinput list-props Asus\ TouchPad | awk '/Device\ Enabled/ {print $NF}')"
+if [[ isenabled -eq 1 ]]
+then
+	xinput disable "$touchpadname"
+else
+	xinput enable "$touchpadname"
+fi
