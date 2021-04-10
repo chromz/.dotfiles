70ec7efb57662727fae99bed0621286a23298c3f
Rodrigo <rodrigo@custodiovargas.com>
2021-04-10 10:11:22 -0600

    New keyboard layout

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code Nerd Font:size=9:antialias=true:autohint=true:weight=medium";
+static char *font = "Fira Code Nerd Font:size=11:antialias=true:autohint=true:weight=medium";
 static int borderpx = 2;
 
 /*
