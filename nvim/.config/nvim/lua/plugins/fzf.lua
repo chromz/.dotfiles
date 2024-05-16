6e42dbbc388a5db82b515e54cea293e751efaa61
Rodrigo <rodrigo@custodiovargas.com>
2024-05-16 11:37:25 -0600

    feat: Added rg opts to fzf

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -2,7 +2,11 @@ return {
   'ibhagwan/fzf-lua',
   config = function()
     local fzf = require('fzf-lua')
-    fzf.setup()
+    fzf.setup({
+      grep = {
+        rg_opts = "--column --hidden --line-number --no-heading --color=always --smart-case --max-columns=4096 -e",
+      },
+    })
     vim.keymap.set('n', '<C-p>', function()
       fzf.files()
     end, {})
