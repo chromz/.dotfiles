6265aaf87673a807e5507865ec635f3df87e0926
Rodrigo <rodrigo@custodiovargas.com>
2024-06-12 15:32:38 -0600

    feat(Nvim): Added folding

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -20,5 +20,16 @@ return {
         additional_vim_regex_highlighting = false,
       },
     }
+    vim.opt.foldlevel = 20
+    vim.api.nvim_create_autocmd({ "FileType" }, {
+        callback = function()
+          if require("nvim-treesitter.parsers").has_parser() then
+            vim.opt.foldmethod = "expr"
+            vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+          else
+            vim.opt.foldmethod = "syntax"
+          end
+        end,
+    })
   end
 }
